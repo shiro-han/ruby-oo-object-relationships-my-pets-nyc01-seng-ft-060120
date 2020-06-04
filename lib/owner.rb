6de2faeedaea9a0f8dfef9a118e91d184614a382
RitaHartnell <mel.soriano6123@gmail.com>
2020-06-04 14:29:11 -0400

    version 2

--- lib/owner.rb
+++ lib/owner.rb
@@ -9,6 +9,56 @@ class Owner
   def say_species
     "I am a #{@species}."
   end
+
+  def cats
+    Cat.all.select{ |cat|
+      cat.owner == self
+    }
+  end
+
+  def dogs
+    Dog.all.select{ |doggo|
+      doggo.owner == self
+    }
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.map{ |doggo|
+      doggo.mood = "happy"
+    }
+  end
+
+  def feed_cats
+    self.cats.map{ |kitteh|
+      kitteh.mood = "happy"
+    }
+  end
+
+  def sell_pets
+    self.dogs.map{ |dog|
+      dog.owner = nil
+      dog.mood = "nervous"
+    }
+
+    self.cats.map{ |cat|
+      cat.owner = nil
+      cat.mood = "nervous"
+    }
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
+
   def self.all
     @@all
   end
@@ -18,4 +68,8 @@ class Owner
   def self.reset_all
     @@all.clear
   end
+
+
+
+
 end
