b21b954e6f31881c104789856d1fae7fbc4d8fb2
shiro-han <shan99business@gmail.com>
2020-06-04 14:10:06 -0400

    version 1

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,21 @@
 class Owner
-  # code goes here
+  @@all = []
+  attr_reader :name, :species
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  def say_species
+    "I am a #{@species}."
+  end
+  def self.all
+    @@all
+  end
+  def self.count
+    @@all.length
+  end
+  def self.reset_all
+    @@all.clear
+  end
 end
